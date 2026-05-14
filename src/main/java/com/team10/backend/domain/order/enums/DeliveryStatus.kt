422e44ef99f630b29984a2f47107aff838b9068a
jowoojin <codewrite123@gmail.com>
2026-05-14 20:40:39 +0900

    refactor(enum): DeliveryStatus 코틀린 마이그레이션

--- src/main/java/com/team10/backend/domain/order/enums/DeliveryStatus.kt
+++ src/main/java/com/team10/backend/domain/order/enums/DeliveryStatus.kt
@@ -1,7 +1,7 @@
-package com.team10.backend.domain.order.enums;
+package com.team10.backend.domain.order.enums
 
-public enum DeliveryStatus {
-    READY,     // 배송 준비 중 (결제 완료 후 생성)
+enum class DeliveryStatus {
+    READY,  // 배송 준비 중 (결제 완료 후 생성)
     SHIPPING,  // 배송 중
-    COMPLETED  // 배송 완료
+    COMPLETED // 배송 완료
 }
