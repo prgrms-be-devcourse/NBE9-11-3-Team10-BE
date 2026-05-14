7077238b2d46be66d5df419f2ffb2954dba97d17
jowoojin <codewrite123@gmail.com>
2026-05-14 16:55:06 +0900

    refactor(dto) : CancelRequest 마이그레이션

--- src/main/java/com/team10/backend/domain/order/dto/cancel/CancelRequest.kt
+++ src/main/java/com/team10/backend/domain/order/dto/cancel/CancelRequest.kt
@@ -1,6 +1,6 @@
-package com.team10.backend.domain.order.dto.cancel;
+package com.team10.backend.domain.order.dto.cancel
 
-public record CancelRequest(
-        String cancelReason          // 취소 사유 (필수)
-//        Long cancelAmount            // 취소할 금액 (부분 취소 시 필수, 누락 시 전액 취소)
-) {}
+
+data class CancelRequest(
+    val cancelReason: String // 취소 사유 (필수)
+)
