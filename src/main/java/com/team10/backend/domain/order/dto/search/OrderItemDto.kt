dc5c2913575dd3dec972e7fe5a64cf32eb5b6cca
jowoojin <codewrite123@gmail.com>
2026-05-14 17:12:26 +0900

    refactor(dto) : OrderItemDto 마이그레이션

--- src/main/java/com/team10/backend/domain/order/dto/search/OrderItemDto.kt
+++ src/main/java/com/team10/backend/domain/order/dto/search/OrderItemDto.kt
@@ -1,19 +1,22 @@
-package com.team10.backend.domain.order.dto.search;
+package com.team10.backend.domain.order.dto.search
 
-import com.team10.backend.domain.order.entity.OrderProducts;
+import com.team10.backend.domain.order.entity.OrderProducts
 
-record OrderItemDto(
-        Long productId,
-        String productName,
-        int quantity,
-        int orderPrice
+data class OrderItemDto(
+    val productId: Long,
+    val productName: String,
+    val quantity: Int,
+    val orderPrice: Int
 ) {
-    public static OrderItemDto from(OrderProducts op) {
-        return new OrderItemDto(
-                op.getProduct().getId(),
-                op.getProduct().getProductName(),
-                op.getQuantity(),
-                op.getOrderPrice()
-        );
+    companion object {
+        fun from(op: OrderProducts): OrderItemDto {
+            // Property 접근법을 사용하여 코드를 간결하게 유지
+            return OrderItemDto(
+                productId = op.product.id,
+                productName = op.product.productName,
+                quantity = op.quantity,
+                orderPrice = op.orderPrice
+            )
+        }
     }
 }
