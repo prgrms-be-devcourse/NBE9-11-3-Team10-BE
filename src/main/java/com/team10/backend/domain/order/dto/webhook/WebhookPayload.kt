5c96fb54d9281f82567e8f25c703060fd442e7af
jowoojin <codewrite123@gmail.com>
2026-05-14 17:03:31 +0900

    refactor(dto) : WebhookPayload 마이그레이션

--- src/main/java/com/team10/backend/domain/order/dto/webhook/WebhookPayload.kt
+++ src/main/java/com/team10/backend/domain/order/dto/webhook/WebhookPayload.kt
@@ -1,14 +1,14 @@
-package com.team10.backend.domain.order.dto.webhook;
+package com.team10.backend.domain.order.dto.webhook
 
 //https://docs.tosspayments.com/reference/using-api/webhook-events#payment_status_changed
-public record WebhookPayload(
-        String eventType, // PAYMENTS_STATUS_CHANGED 등
-        Data data
+data class WebhookPayload(
+    val eventType: String, // 웹훅의 목적이므로 반드시 존재해야 함
+    val data: Data
 ) {
-    public record Data(
-            String paymentKey,
-            String orderId,
-            String status, // DONE, CANCELED, PARTIAL_CANCELED 등
-            Long totalAmount
-    ) {}
+    data class Data(
+        val paymentKey: String, // 결제 식별자 (필수)
+        val orderId: String,    // 주문 식별자 (필수)
+        val status: String,     // 결제 상태 (필수)
+        val totalAmount: Long   // 결제 금액 (필수)
+    )
 }
