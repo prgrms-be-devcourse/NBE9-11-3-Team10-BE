c1d6969faabd98d41c3338848af3f747b4b9a760
jowoojin <codewrite123@gmail.com>
2026-05-15 15:28:38 +0900

    refactor(repo): OrderDeliveryRepository 코틀린 변환

--- src/main/java/com/team10/backend/domain/order/repository/OrderDeliveryRepository.kt
+++ src/main/java/com/team10/backend/domain/order/repository/OrderDeliveryRepository.kt
@@ -1,7 +1,7 @@
-package com.team10.backend.domain.order.repository;
+package com.team10.backend.domain.order.repository
 
-import com.team10.backend.domain.order.entity.OrderDelivery;
-import org.springframework.data.jpa.repository.JpaRepository;
+import com.team10.backend.domain.order.entity.OrderDelivery
+import org.springframework.data.jpa.repository.JpaRepository
 
-public interface OrderDeliveryRepository extends JpaRepository<OrderDelivery,Long> {
+interface OrderDeliveryRepository : JpaRepository<OrderDelivery, Long> {
 }
