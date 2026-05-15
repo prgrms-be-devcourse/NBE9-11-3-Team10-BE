4a02d0aceb6672608faa36ec6a97e740147d0fe8
lih8697-1 <lih8697@gmail.com>
2026-05-15 14:59:43 +0900

    refactor: Product Repository Kotlin 변환

---
+++ src/main/java/com/team10/backend/domain/product/repository/ProductRepository.kt
@@ -0,0 +1,25 @@
+package com.team10.backend.domain.product.repository
+
+import com.team10.backend.domain.product.entity.Product
+import com.team10.backend.domain.product.enums.ProductStatus
+import com.team10.backend.domain.product.enums.ProductType
+import jakarta.persistence.LockModeType
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor
+import org.springframework.data.jpa.repository.Lock
+import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.query.Param
+import java.util.*
+
+interface ProductRepository : JpaRepository<Product, Long>, JpaSpecificationExecutor<Product> {
+    fun findByType(type: ProductType, pageable: Pageable): Page<Product>
+    fun findByStatus(status: ProductStatus, pageable: Pageable): Page<Product>
+    fun findByStatusNot(status: ProductStatus, pageable: Pageable): Page<Product>
+    fun findByTypeAndStatus(type: ProductType, status: ProductStatus, pageable: Pageable): Page<Product>
+
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("select p from Product p where p.id = :productId")
+    fun findByIdWithPessimisticLock(@Param("productId") productId: Long): Optional<Product>
+}
