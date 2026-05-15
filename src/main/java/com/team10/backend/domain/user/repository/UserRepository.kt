b89ee34cb9025f0e36f5fafd194903177bb5a392
moon-three <moondal366@gmail.com>
2026-05-15 16:30:50 +0900

    refactor(repository): Optional 제거

--- src/main/java/com/team10/backend/domain/user/repository/UserRepository.kt
+++ src/main/java/com/team10/backend/domain/user/repository/UserRepository.kt
@@ -3,15 +3,11 @@ package com.team10.backend.domain.user.repository
 import com.team10.backend.domain.user.entity.User
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.Query
-import java.util.*
 
 interface UserRepository : JpaRepository<User, Long> {
     fun existsByEmail(email: String): Boolean
     fun existsByNickname(nickname: String): Boolean
-
-    // TODO: Optional 제거 예정
-    fun findByEmail(email: String): Optional<User>
-
+    fun findByEmail(email: String): User?
 
     @Query(
         """
