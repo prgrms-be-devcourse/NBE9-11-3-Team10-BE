874cf1725173bbb17059d14896fa8cebd37c8f12
moon-three <moondal366@gmail.com>
2026-05-15 10:04:09 +0900

    refactor(repository): Auth/User Repository 마이그레이션

--- src/main/java/com/team10/backend/domain/auth/repository/RefreshTokenRepository.kt
+++ src/main/java/com/team10/backend/domain/auth/repository/RefreshTokenRepository.kt
@@ -1,10 +1,10 @@
-package com.team10.backend.domain.auth.repository;
+package com.team10.backend.domain.auth.repository
 
-import com.team10.backend.domain.auth.entity.RefreshToken;
-import org.springframework.data.jpa.repository.JpaRepository;
+import com.team10.backend.domain.auth.entity.RefreshToken
+import org.springframework.data.jpa.repository.JpaRepository
+import java.util.Optional
 
-import java.util.Optional;
-
-public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
-    Optional<RefreshToken> findByToken(String token);
+interface RefreshTokenRepository : JpaRepository<RefreshToken, Long> {
+    // TODO: Optional 제거
+    fun findByToken(token: String): Optional<RefreshToken>
 }
