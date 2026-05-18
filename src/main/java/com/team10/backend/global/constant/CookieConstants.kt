0644edc9d9f9fbc3f4fbf0a6a405a2a6ec9f48fe
moon-three <moondal366@gmail.com>
2026-05-18 09:48:09 +0900

    refactor(constant): Constants 마이그레이션

--- src/main/java/com/team10/backend/global/constant/CookieConstants.kt
+++ src/main/java/com/team10/backend/global/constant/CookieConstants.kt
@@ -1,9 +1,6 @@
-package com.team10.backend.global.constant;
+package com.team10.backend.global.constant
 
-public final class CookieConstants {
-    private CookieConstants() {}
-
-    public static final String ACCESS_TOKEN = "accessToken";
-    public static final String REFRESH_TOKEN = "refreshToken";
-
+object CookieConstants {
+    const val ACCESS_TOKEN: String = "accessToken"
+    const val REFRESH_TOKEN: String = "refreshToken"
 }
--- src/main/java/com/team10/backend/global/constant/JwtConstants.kt
+++ src/main/java/com/team10/backend/global/constant/JwtConstants.kt
@@ -1,7 +1,5 @@
-package com.team10.backend.global.constant;
+package com.team10.backend.global.constant
 
-public final class JwtConstants {
-    private JwtConstants() {}
-
-    public static final String CLAIMS_ROLE = "role";
+object JwtConstants {
+    const val CLAIMS_ROLE: String = "role"
 }
