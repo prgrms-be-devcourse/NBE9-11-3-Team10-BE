adce21fe101ea61e740fa65fe96477c57a6fde26
moon-three <moondal366@gmail.com>
2026-05-18 11:25:59 +0900

    refactor(CustomUserPrincipal): principal 객체 마이그레이션
    # Conflicts:
    #	src/main/java/com/team10/backend/domain/order/controller/OrderController.java

--- src/main/java/com/team10/backend/global/security/CustomUserPrincipal.kt
+++ src/main/java/com/team10/backend/global/security/CustomUserPrincipal.kt
@@ -1,8 +1,8 @@
-package com.team10.backend.global.security;
+package com.team10.backend.global.security
 
-import com.team10.backend.domain.user.enums.Role;
+import com.team10.backend.domain.user.enums.Role
 
-public record CustomUserPrincipal(
-        Long userId,
-        Role role
-) {}
+data class CustomUserPrincipal(
+    @JvmField val userId: Long,
+    @JvmField val role: Role
+)
