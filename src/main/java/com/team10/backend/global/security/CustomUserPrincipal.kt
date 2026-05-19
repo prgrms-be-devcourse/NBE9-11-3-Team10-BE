3c0cae77ab1249e24e2eaa74943b397278636220
moon-three <moondal366@gmail.com>
2026-05-19 15:15:47 +0900

    refactor(test): TokenProviderTest 마이그레이션

--- src/main/java/com/team10/backend/global/security/CustomUserPrincipal.kt
+++ src/main/java/com/team10/backend/global/security/CustomUserPrincipal.kt
@@ -3,6 +3,6 @@ package com.team10.backend.global.security
 import com.team10.backend.domain.user.enums.Role
 
 data class CustomUserPrincipal(
-    @JvmField val userId: Long,
-    @JvmField val role: Role
+    val userId: Long,
+    val role: Role
 )
